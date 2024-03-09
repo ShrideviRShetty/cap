53416839ef54d8183274b14ba30ae30541146eb0
ShrideviRShetty <shrishetty2302@gmail.com>
2024-03-09 13:55:24 +0530

    v1.1.1

---
+++ head.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.querySelector('.nav');
+    menu.classList.toggle('show');
+    var hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+}
